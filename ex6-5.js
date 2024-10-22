bc7b707abeaab1be5c1286a273e22fc3e5319fd9
takukuda <167833203+takukuda@users.noreply.github.com>
2024-10-22 10:30:05 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,5 @@
 var who65='world';
-function sayhello65() {alert('hello,' + who65 + '!')};
+function sayhello65() {alert('hello,' + who65 + '!');}
 function taro65(){who65 = '太郎';}
 function hanako65(){who65 = '花子';}
+function come65(){alert(who65 + 'が来た')};
