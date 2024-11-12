cc48930521e784d7ad78a2755ff055d3c2c6c270
takukuda <167833203+takukuda@users.noreply.github.com>
2024-11-12 10:09:23 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,3 +1,4 @@
 function sayhello68(){alert('Hello,'+ document.getElementById('who68').innerHTML+'!');}
 function taro68(){document.getElementById('who68').innerHTML = '太郎';}
 function hanako68(){document.getElementById('who68').innerHTML = '花子';}
+function gazouhenkou68(){document.getElementById('gazou68').innerHTML = './image/buranko_girl_smile.png';}
