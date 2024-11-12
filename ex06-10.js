932a3e92e6aafe97e1c9ab2caf0592f1a5fce010
takukuda <167833203+takukuda@users.noreply.github.com>
2024-11-12 10:33:58 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/buranko_girl_smile.png;}
-function imgchange2(){document.getElementById('logo').src='./image/buranko_girl_sad.png;}
+function imgchange1(){document.getElementById('logo').src='./image/buranko_girl_smile.png';}
+function imgchange2(){document.getElementById('logo').src='./image/buranko_girl_sad.png';}
