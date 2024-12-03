1151cdafd148eeceb0899de65dfcf4d67ed95268
takukuda <167833203+takukuda@users.noreply.github.com>
2024-12-03 10:06:14 +0900

    Update ex06-13.js

--- ex06-13.js
+++ ex06-13.js
@@ -1,7 +1,7 @@
 function mes(){alert("3秒経ちました！");}
 function hyoji13(){
   //表示する文字
-  var str = "一文字ずつ表示します。";
+  var str13 = "一文字ずつ表示します。";
   //テキストボックスの文字数
   var cnt13 = document.timer13.moji13.value.length;
   //文字が全部表示されているか確認
