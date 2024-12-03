341ef6bf473d74db3a91cc7bb606687e982a8783
takukuda <167833203+takukuda@users.noreply.github.com>
2024-12-03 10:30:37 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,4 +1,4 @@
 function syashin1(){document.getElementById('smile').src='./image/buranko_girl_smile.png';
-                   setTimeout("syashin2()"),3000);}
+                   setTimeout("syashin2()",3000);}
 
 function syashin2(){document.getElementById('smile').src='./image/buranko_girl_sad.png';}
