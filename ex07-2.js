c8dfa90e92cd49f79bec49197b8eb1c56adc1d1c
takukuda <167833203+takukuda@users.noreply.github.com>
2024-12-10 10:18:22 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,4 +1,4 @@
 function add(){
   var x = document.getElementById('input1').value;
   var y = document.getElementById('input2').value;
-  alert(Number(x)+Number(y);}
+  alert(Number(x)+Number(y));}
