fe349aa2f727812ca46862b92a9f8b29b3dc5397
takukuda <167833203+takukuda@users.noreply.github.com>
2024-12-24 10:43:03 +0900

    Update ex07-7.js

--- ex07-7.js
+++ ex07-7.js
@@ -1,3 +1,3 @@
 function nazo77(){if(document.getElementById('answer77').value == '夢')
 {alert('あたり！')} 
-else if (document.getElementById('answer77').value == 'ゆめ') {alert('あたり！');} else {alert('はずれ！'}}
+else if (document.getElementById('answer77').value == 'ゆめ') {alert('あたり！');} else {alert('はずれ！')}}
