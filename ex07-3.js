4ff095232790179706989c8b9a9e7f49452432c4
takukuda <167833203+takukuda@users.noreply.github.com>
2025-01-07 09:58:31 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -3,5 +3,5 @@ function bmi(){
    var y = document.getElementById('taiju').value;
    if ((Number(y)/(Number(x)*Number(x)))<18.5) {alert (Number(y)/(Number(x)*Number(x))) + 'kg/m²'+' 低体重'}
    else if((Number(y)/(Number(x)*Number(x)))<25) {alert(Number(y)/(Number(x)*Number(x))) + 'kg/m²'+' 普通'}
-   else {alert(Number(y)/(Number(x)*Number(x))) + 'kg/m²'+' 肥満'}
+   else {alert(Number(y)/(Number(x)*Number(x))) + 'kg/m²'+' 肥満'}}
   
