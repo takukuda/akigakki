a8f0182931c06f65270ba6b97e730e7f4cae6fac
takukuda <167833203+takukuda@users.noreply.github.com>
2025-01-07 09:51:50 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,6 +1,6 @@
 function bmi(){
    var x = document.getElementById('shintyou').value;
    var y = document.getElementById('taiju').value;
-   if {(Number(y)/(Number(x)*Number(x)))<18.5} (alert (Number(y)/(Number(x)*Number(x))) + 'kg/m²')
+   if ((Number(y)/(Number(x)*Number(x)))<18.5) {alert (Number(y)/(Number(x)*Number(x))) + 'kg/m²'+' 低体重'}
    
   
